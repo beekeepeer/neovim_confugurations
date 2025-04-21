c19bbf3998fd25109cfbffd783b45142aa72b828
Dmitri Goriuc <goriucdmitri@gmail.com>
2025-04-21 16:07:49 +0200

    update
    Formating form intellij idea; Java 24 as main jdk;

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Global indent settings
+vim.opt.expandtab   = true    -- use spaces, not tabs
+vim.opt.tabstop     = 4       -- a real TAB == 4 spaces
+vim.opt.shiftwidth  = 4       -- auto‑indent uses 4 spaces
