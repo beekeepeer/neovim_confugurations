87829f080ec700b781f18d3db42f57e129c4965e
Dmitri Goriuc <goriucdmitri@gmail.com>
2026-02-09 18:28:52 +0100

    obsidian.nvim works

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -6,19 +6,67 @@ return {
       { "<leader>o", group = "Obsidian", icon = "󱓧 " },
     })
   end,
-  version = "*", -- use latest release, remove to use latest commit
+  version = "*", 
   ft = "markdown",
   ---@module 'obsidian'
   ---@type obsidian.config
   opts = {
-    legacy_commands = false, -- this will be removed in the next major release
+    legacy_commands = false, 
     workspaces = {
       {
         name = "ObsidianVault",
         path = "~/Documents/ObsidianVault/",
       },
     },
-  },
+    templates = {
+      subdir = "Templates", 
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+    },
+
+
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- If a title is provided (e.g., [[People]]), use it as the filename
+      if title ~= nil then
+        -- Optional: Clean up the title to be file-system friendly
+        return title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- Fallback to a random ID if no title is provided
+        return tostring(os.time()) .. "-" .. math.random(1000, 9999)
+      end
+    end,
+
+    ---@param note obsidian.Note
+    frontmatter = {
+      ---@param note obsidian.Note
+      func = function(note)
+        -- 1. Initialize 'out' without the 'id' field
+        local out = { 
+          aliases = note.aliases, 
+          tags = note.tags 
+        }
+
+        -- Separate Date and Time for better organization
+        out.date = os.date("%Y-%m-%d")
+        out.time = os.date("%H:%M")
+
+        -- 3. (Optional) If the note has a title, include it
+        if note.title then
+          out.title = note.title
+        end
+
+        -- 4. Merge metadata from templates
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+
+        return out
+      end,
+    },  },
   keys = {
     -- Top Level / Navigation
     { "<leader>on", "<cmd>Obsidian new<cr>", desc = "New Note" },
