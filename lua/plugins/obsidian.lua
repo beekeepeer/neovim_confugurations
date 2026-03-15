8ee3845697d6b9e5ebfd5d049b375bd5107e3fbc
Dmitri Goriuc <goriucdmitri@gmail.com>
2026-03-15 08:56:01 +0100

    Obsidian on

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -25,20 +25,31 @@ return {
     },
 
 
-    ---@param title string|?
-    ---@return string
-    note_id_func = function(title)
-      if title ~= nil then
-        -- Simply return the title exactly as you typed it in the [[link]]
-        return title
-      else
-        return tostring(os.time())
-      end
-    end,
+  -- 2. Configure how new notes are initialized
+  -- If this is set to nil or an empty string, it won't insert any header/YAML.
+  note_id_func = function(title)
+    return title or tostring(os.time())
+  end,
 
+
+    ui = {
+    enable = true, -- Keep UI features but...
+    update_debounce = 200,
+  },
+
+  -- You might also want to ensure the alias matches the title exactly
+  ---@param spec { id: string, title: string|?, alias: string[]|? }
+  ---@return string|table
+  note_path_func = function(spec)
+    -- This ensures the file is saved as "My Note Name.md" 
+    -- instead of "my-note-name.md"
+    local path = spec.dir / tostring(spec.id)
+    return path:with_suffix(".md")
+  end,
 
     ---@param note obsidian.Note
 frontmatter = {
+    enable = false,
   ---@param note obsidian.Note
   func = function(note)
     -- 1. Start with EVERYTHING already in the note's frontmatter
@@ -90,13 +101,31 @@ frontmatter = {
     -- Templates & Help
     { "<leader>om", "<cmd>Obsidian template<cr>", desc = "Insert Template" },
     { "<leader>oN", "<cmd>Obsidian new_from_template<cr>", desc = "New Note from Template" },
-    { "<leader>oh", "<cmd>Obsidian help<cr>", desc = "Obsidian Help" },
+    { "<leader>o?", "<cmd>Obsidian help<cr>", desc = "Obsidian Help" },
     { "<leader>og", "<cmd>Obsidian helpgrep<cr>", desc = "Grep Help" },
 
     -- Integration
     { "<leader>op", "<cmd>Obsidian open<cr>", desc = "Open in Obsidian App" },
 
-    -- Visual Mode Commands
+    -- Visual Mode Commands-- Highlight Selection
+    { 
+      "<leader>oh", 
+      ":s/\\%V.*\\%V./==&==/<cr><cmd>noh<cr>", 
+      mode = "v", 
+      desc = "Highlight Selection (==)" 
+    },
+    { 
+      "<leader>oq", 
+      ":s/\\%V.*\\%V./[[|&]]/<cr><cmd>noh<cr>F|i", 
+      mode = "v", 
+      desc = "Link Selection as Alias" 
+    },
+    { 
+      "<leader>oq", 
+      ":Obsidian link new<cr>", 
+      mode = "v", 
+      desc = "Link Selection to New Note" 
+    },
     { "<leader>on", ":Obsidian extract_note<cr>", mode = "v", desc = "Extract to New Note" },
     { "<leader>ol", ":Obsidian link<cr>", mode = "v", desc = "Link Selection" },
     { "<leader>oL", ":Obsidian link_new<cr>", mode = "v", desc = "Link to New Note" },
